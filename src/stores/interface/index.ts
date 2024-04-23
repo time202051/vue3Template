8b533def994cb30250f5ab87a18503b7bd6da69d
李嘉鹏 <809949003@qq.com>
2024-04-23 20:06:26 +0800

    工作日结束提交

--- src/stores/interface/index.ts
+++ src/stores/interface/index.ts
@@ -1,3 +1,28 @@
+export type LayoutType = "vertical" | "classic" | "transverse" | "columns";
+export type AssemblySizeType = "large" | "default" | "small";
+export type LanguageType = "zh" | "en" | null;
+
+/* GlobalState */
+export interface GlobalState {
+  layout: LayoutType;
+  assemblySize: AssemblySizeType;
+  language: LanguageType;
+  maximize: boolean;
+  primary: string;
+  isDark: boolean;
+  isGrey: boolean;
+  isWeak: boolean;
+  asideInverted: boolean;
+  headerInverted: boolean;
+  isCollapse: boolean;
+  accordion: boolean;
+  breadcrumb: boolean;
+  breadcrumbIcon: boolean;
+  tabs: boolean;
+  tabsIcon: boolean;
+  footer: boolean;
+}
+
 export interface AuthState {
   routeName: string;
   authButtonList: {
